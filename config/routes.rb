e1627ba94869529b82ec3f51ad77053f8a00c750
mukajihh <mukajihh@gmail.com>
2017-05-22 21:45:34 -0300

    add style to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   
-  resources :usuarios
+  # resources :usuarios
   
   root 'sessions#new'
   
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+
+  get '/register' => 'usuarios#new'
+  post '/register' => 'usuarios#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
