2b7eb0a5212e2f41ca63c3e048fe329e7dc46c2c
mukajihh <mukajihh@gmail.com>
2017-05-26 19:52:30 -0300

    remove register configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   
   # resources :usuarios
-  
+
   root 'sessions#new'
-  
+
+  resources 'usuarios'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-
-  get '/register' => 'usuarios#new'
-  post '/register' => 'usuarios#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
