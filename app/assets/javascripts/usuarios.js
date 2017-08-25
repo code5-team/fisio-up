88c70296ede6dc968a295ed64f6ade4b052ecd24
mukajihh <mukajihh@gmail.com>
2017-08-24 23:07:19 -0300

    create styles for homepage

--- app/assets/javascripts/usuarios.js
+++ app/assets/javascripts/usuarios.js
@@ -1,1 +1,19 @@
-$('#calendar').fullCalendar();
+$('#calendar').fullCalendar({
+  header: {
+    right:  'agendaDay,agendaWeek,today prev,next'
+  },
+  events: [
+    {
+      title: 'Event1',
+      start: '2017-08-21',
+      editable: true
+    },
+    {
+      title: 'Event2',
+      start: '2017-08-22',
+      editable: true,
+      color: 'yellow',
+      textColor: 'black'
+    }
+  ]
+});
