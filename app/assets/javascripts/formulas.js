37bcae88e8112fefb4302012d569a511003618aa
mukajihh <mukajihh@gmail.com>
2017-11-09 05:11:20 -0200

    calcs when change value

--- app/assets/javascripts/formulas.js
+++ app/assets/javascripts/formulas.js
@@ -1,61 +1,52 @@
-$('.formula').each(function(obj){
+$('li.formula').each(function(obj){
   $(this).on('click', function(){
-    var index = obj+1;
-    menuSelecionado("#formula-"+index, "#form-"+index);
+    $('li.formula').removeClass('active');
+    $('.form-grid').removeClass('active');
+    $(".resultado").empty();
+
+    $(this).addClass('active');
+    $(".form-grid[data-index='" + $(this).data('index') + "']").addClass('active');
   });
 });
 
 
-$('.btn').each(function(obj){
-  $(this).on('click', function(){
-    var index = obj+1,
-      resultado = $(".resultado-tela");
+$('.input-field input').on('change keyup', function(){
+  var index = $('.form-grid.active').data('index')
+      resultado = $(".resultado");
 
-    switch(index) {
-      case 1:
-        resultado.html(indiceOxigenacao($('#vl1-form1').val(), $('#vl2-form1').val()));
-        break;
-      case 2:
-        resultado.html(oxigenioIdeal($('#vl1-form2').val()));
-        break;
-      case 3:
-        resultado.html(indiceRRS($('#vl1-form3').val(), $('#vl2-form3').val()));
-        break;
-      case 4:
-        resultado.html(complacidadePulmonarEstatica($('#vl1-form4').val(), 
-          $('#vl2-form4').val(), 
-          $('#vl3-form4').val()));
-        break;
-      case 5:
-        resultado.html(complacidadePulmonarDinamica($('#vl1-form5').val(), 
-          $('#vl2-form5').val(),
-          $('#vl3-form5').val())); 
-        break;
-      case 6:
-        resultado.html(elastancia($('#vl1-form6').val(), 
-          $('#vl2-form6').val(),
-          $('#vl3-form6').val()));
-        break;
-      case 7:
-        resultado.html(resistenciaSistemaPulmonar($('#vl1-form7').val(), 
-          $('#vl2-form7').val(),
-          $('#vl3-form7').val()));
-        break;
-      case 8:
-        resultado.html(pressaoInspiratoriaMaximaHomens($('#vl1-form8').val()));
-        break;
-      case 9:
-        resultado.html(pressaoInspiratoriaMaximaMulheres($('#vl1-form9').val()));
-        break;
-      case 10:
-        resultado.html(pressaoRespiratoriaMaximaHomens($('#vl1-form10').val()));
-        break;
-      case 11:
-        resultado.html(pressaoRespiratoriaMaximaMulheres($('#vl1-form11').val()));
-        break;          
-    }
-    
-    
-
-  });
+  switch(index) {
+    case 1:
+      resultado.html(indiceOxigenacao($('#vl1-form1').val(), $('#vl2-form1').val()));
+      break;
+    case 2:
+      resultado.html(oxigenioIdeal($('#vl1-form2').val()));
+      break;
+    case 3:
+      resultado.html(indiceRRS($('#vl1-form3').val(), $('#vl2-form3').val()));
+      break;
+    case 4:
+      resultado.html(complacidadePulmonarEstatica($('#vl1-form4').val(), $('#vl2-form4').val(), $('#vl3-form4').val()));
+      break;
+    case 5:
+      resultado.html(complacidadePulmonarDinamica($('#vl1-form5').val(), $('#vl2-form5').val(), $('#vl3-form5').val()));
+      break;
+    case 6:
+      resultado.html(elastancia($('#vl1-form6').val(), $('#vl2-form6').val(), $('#vl3-form6').val()));
+      break;
+    case 7:
+      resultado.html(resistenciaSistemaPulmonar($('#vl1-form7').val(), $('#vl2-form7').val(), $('#vl3-form7').val()));
+      break;
+    case 8:
+      resultado.html(pressaoInspiratoriaMaximaHomens($('#vl1-form8').val()));
+      break;
+    case 9:
+      resultado.html(pressaoInspiratoriaMaximaMulheres($('#vl1-form9').val()));
+      break;
+    case 10:
+      resultado.html(pressaoRespiratoriaMaximaHomens($('#vl1-form10').val()));
+      break;
+    case 11:
+      resultado.html(pressaoRespiratoriaMaximaMulheres($('#vl1-form11').val()));
+      break;
+  }
 });
--- app/assets/javascripts/todasAsFuncoes.js
+++ app/assets/javascripts/todasAsFuncoes.js
@@ -1,137 +1,109 @@
-function menuSelecionado(classeDaLista, formDaLista){
-  $('.formula').removeClass('active');
-  $('.form-grid').removeClass('active');
-  $(".resultado-tela").empty();
+var error = "Valores incorretos para calculo!";
 
-  $(classeDaLista).addClass('active');
-
-  $(formDaLista).addClass('active');
-  //console.log(classeDaLista);
-  //console.log(formDaLista);
-}
-
-
-function indiceOxigenacao(pao, fio){
-  if(pao > 0 && fio > 0){
+function indiceOxigenacao(pao, fio) {
+  if (pao > 0 && fio > 0) {
    result = pao / fio;
 
-   if(result > 300)
-      return "Resultado: "+ result + ". Normalidade"
-    else if(result > 200 && result <= 300)
-      return "Resultado: "+ result + ". Lesão Pulmonar Aguda (LPA)"
+   if (result > 300)
+      return "Resultado: "+ result.toFixed(2) + ". Normalidade"
+    else if (result > 200 && result <= 300)
+      return "Resultado: "+ result.toFixed(2) + ". Lesão Pulmonar Aguda (LPA)"
     else (result <= 200)
-      return "Resultado: "+ result + ". Lesão Pulmonar Grave (SARA)"
-
-  } else
-      return "Valores incorretos para calculo!";
-
-  //Resultados:
-  //           result >= 300   Normalidade
-  //    200 >= result <= 300   Lesão Pulmonar Aguda (LPA)
-  //           result <= 200   Lesão Pulmonar Grave (SARA)
+      return "Resultado: "+ result.toFixed(2) + ". Lesão Pulmonar Grave (SARA)"
+  } else {
+    return error;
+  }
 }
 
-
-//Fórmula Pressão Parcial de Oxigênio Ideal
-// oxigenioIdeal
-function oxigenioIdeal(idade){
-  if (130 > idade && idade > 0){
+function oxigenioIdeal(idade) {
+  if (130 > idade && idade > 0) {
     result = 109 - (idade * 0.43);
 
-    if(result <= 80)
+    if (result <= 80)
       return "O seu PaO2 é de "+ result.toFixed(2) +". Está em condições normais."
     else
       return "O seu PaO2 é de "+ result.toFixed(2) +". Não está em condições normais.";
+  } else {
+    return error;
   }
-  else
-    return "Valores incorretos para calculo!";
-  // Resultados: A PaO2 ideal é por volta 80mmHg, pois garante uma saturação
-  // de 95% em condições normais
 }
 
-
-function indiceRRS(fr, vc){
-  if (fr > 0 && vc > 0){
+function indiceRRS(fr, vc) {
+  if (fr > 0 && vc > 0) {
     result = fr / vc;
     
-    if(result <= 105)
+    if (result <= 105)
       return "IRPM: "+ result.toFixed(2) +". Normalidade.";
     else
       return "IRPM: "+ result.toFixed(2) +". Não indicado, Iniciação de desmame.";
+  } else{
+    return error;
   }
-  else
-    return "Valores incorretos para calculo!";
-
 }
 
-
-function complacidadePulmonarEstatica (volcorrente, pressaoplato, peep){
-  if (volcorrente > 0 && pressaoplato > 0 && peep > 0){
+function complacidadePulmonarEstatica(volcorrente, pressaoplato, peep) {
+  if (volcorrente > 0 && pressaoplato > 0 && peep > 0) {
     result = volcorrente / (pressaoplato - peep);
-    
-    if ((result == 0) || (result == Infinity))
+
+    if (result == 0 || result == Infinity)
       return "O Volume corrente não pode ser dividido por 0!! Altere os valores da fórmula.";
-    
     else
-      if((result >= 50) || (result <= 100))
+      if (result >= 50 || result <= 100)
         return "CPE: "+result.toFixed(2)+". Normalidade";
       else
-        return "CPE: "+result.toFixed(2)+". Anormalidade";  
-  } 
-  else
-    return "Valores incorretos para calculo!";
+        return "CPE: "+result.toFixed(2)+". Anormalidade";
+  } else {
+    return error;
+  }
 }
 
-function complacidadePulmonarDinamica (volcorrente, pressaoplato, peep){
-  if (volcorrente > 0 && pressaoplato > 0 && peep > 0){
+function complacidadePulmonarDinamica(volcorrente, pressaoplato, peep) {
+  if (volcorrente > 0 && pressaoplato > 0 && peep > 0) {
     result = volcorrente / (pressaoplato - peep);
-    
-    if ((result == 0) || (result == Infinity))
+
+    if (result == 0 || result == Infinity)
       return "O Volume corrente não pode ser dividido por 0!! Altere os valores da fórmula.";
-   
     else
-      if((result >= 100) || (result <= 200))      
+      if (result >= 100 || result <= 200)
         return "CPD: "+result.toFixed(2)+". Normalidade";
       else
         return "CPD: "+result.toFixed(2)+". Anormalidade";
-  }  
-  else
-    return "Valores incorretos para calculo!";
-}
-
-
-function elastancia(pressaoplato, peep, volcorrente){
-  if (volcorrente > 0 && pressaoplato > 0 && peep > 0){
-    result = (pressaoplato - peep) / volcorrente;
-      return "Valor da Elastancia: "+result.toFixed(2)+".";
+  } else {
+    return error;
   }
-  else
-    return "Valores incorretos para calculo!";
 }
 
+function elastancia(pressaoplato, peep, volcorrente) {
+  if (volcorrente > 0 && pressaoplato > 0 && peep > 0) {
+    result = (pressaoplato - peep) / volcorrente;
 
-function resistenciaSistemaPulmonar (pressaopico, pressaoplato, fluxo){
-  if(pressaopico > 0 && pressaoplato > 0 && fluxo > 0){
+    return "Valor da Elastancia: "+result.toFixed(2)+".";
+  } else {
+    return error;
+  }
+}
+
+function resistenciaSistemaPulmonar(pressaopico, pressaoplato, fluxo) {
+  if (pressaopico > 0 && pressaoplato > 0 && fluxo > 0) {
     result = (pressaopico - pressaoplato) / fluxo;
-      
-    if ((result == 0) || (result == Infinity))
+
+    if (result == 0 || result == Infinity)
       return "O Volume corrente não pode ser dividido por 0!! Altere os valores da fórmula.";
-     
-      else
-        if((result >= 4) || (result <= 7))      
-          return "RSP: "+result.toFixed(2)+". Normalidade";
-        else
-          return "RSP: "+result.toFixed(2)+". Anormalidade";
-    }  
     else
-      return "Valores incorretos para calculo!";
+      if (result >= 4 || result <= 7)
+        return "RSP: "+result.toFixed(2)+". Normalidade";
+      else
+        return "RSP: "+result.toFixed(2)+". Anormalidade";
+  } else {
+    return error;
+  }
 }
 
-function pressaoInspiratoriaMaximaHomens (idade){
-  if (130 > idade && idade > 0){
+function pressaoInspiratoriaMaximaHomens(idade) {
+  if (130 > idade && idade > 0) {
     result = (0.80 * idade) + 155.3;
 
-    if(result <= -20)
+    if (result <= -20)
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Falência muscular inspiratória.";
     else if (result >= -25 && result <= -40)
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Fadiga muscular respiratória.";
@@ -139,16 +111,16 @@ function pressaoInspiratoriaMaximaHomens (idade){
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Fraqueza muscular";
     else
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Força muscular inspiratória normal";
-  } 
-  else
-    return "Valores incorretos para calculo!";
+  } else {
+    return error;
+  }
 }
 
-function pressaoInspiratoriaMaximaMulheres (idade){
-  if (130 > idade && idade > 0){
+function pressaoInspiratoriaMaximaMulheres(idade) {
+  if (130 > idade && idade > 0) {
     result = (0.49 * idade) + 110.4;
 
-    if(result <= -20)
+    if (result <= -20)
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Falência muscular inspiratória.";
     else if (result >= -25 && result <= -40)
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Fadiga muscular respiratória.";
@@ -156,34 +128,34 @@ function pressaoInspiratoriaMaximaMulheres (idade){
       return "Pressao Inspiratoria: "+result.toFixed(2)+". Fraqueza muscular";
     else
      return "Pressao Inspiratoria: "+result.toFixed(2)+". Força muscular inspiratória normal";
+  } else {
+    return error;
   }
-  else
-    return "Valores incorretos para calculo!";
 }
 
 
-function pressaoRespiratoriaMaximaHomens (idade){
-  if (130 > idade && idade > 0){
+function pressaoRespiratoriaMaximaHomens(idade) {
+  if (130 > idade && idade > 0) {
     result = (0.81 * idade) + 165.3;
 
-    if(result >= 100 && result <= 150)
+    if (result >= 100 && result <= 150)
       return "Pressão Respiratoria Normal. Resultado "+result.toFixed(2)+".";
     else
      return "Pressão Respiratória Anormal. Resultado "+result.toFixed(2)+".";
+  } else {
+    return error;
   }
-  else
-    return "Valores incorretos para calculo!";
 }
 
-function pressaoRespiratoriaMaximaMulheres (idade){
-  if (130 > idade && idade > 0){
+function pressaoRespiratoriaMaximaMulheres(idade) {
+  if (130 > idade && idade > 0) {
     result = (0.61 * idade) + 115.6;
 
-    if(result >= 100 && result <= 150)
+    if (result >= 100 && result <= 150)
       return "Pressao Respiratoria Normal. Resultado "+result.toFixed(2)+".";
     else
      return "Pressao Respiratória Anormal. Resultado "+result.toFixed(2)+".";
+  } else {
+    return error;
   }
-  else
-    return "Valores incorretos para calculo!";
 }
