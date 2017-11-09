46bf4f7b03c4608ca794329d3756e28576a0481f
mukajihh <mukajihh@gmail.com>
2017-11-09 08:54:00 -0200

    create report by unit

--- app/controllers/relatorio_controller.rb
+++ app/controllers/relatorio_controller.rb
@@ -1,6 +1,7 @@
 class RelatorioController < ApplicationController
   def index
     @usuario = helpers.current_user
+    @unidades = Unidade.all
     @equipe = []
     for usuario in Usuario.all
       if usuario.ativo?
@@ -11,18 +12,29 @@ class RelatorioController < ApplicationController
 
   def consulta
     @usuario = helpers.current_user
-    @unidade = Unidade.all
+    @unidades= Unidade.all
 
     @get = params[:get]
     @datainicio = @get['datainicio']
     @datafim = @get['datafim']
 
     @with_user = params['with-user'] == '1' ? true : false
+    @with_unit = params['with-unit'] == '1' ? true : false
     if @with_user
       @membro = Usuario.find(@get['usuario'])
-      @plantoes = find_plantao_with_user(@membro, @datainicio, @datafim)
+      if @with_unit
+        @unidade = Unidade.find(@get['unidade'])
+        @plantoes = find_plantao_full(@membro, @unidade, @datainicio, @datafim)
+      else
+        @plantoes = find_plantao_with_user(@membro, @datainicio, @datafim)
+      end
     else
-      @plantoes = find_plantao(@datainicio, @datafim)
+      if @with_unit
+        @unidade = Unidade.find(@get['unidade'])
+        @plantoes = find_plantao_with_unit(@unidade, @datainicio, @datafim)
+      else
+        @plantoes = find_plantao(@datainicio, @datafim)
+      end
     end
     @total_plantoes = @plantoes.length
 
@@ -53,5 +65,19 @@ class RelatorioController < ApplicationController
     data_fim = DateTime.parse(data_fim, '%Y-%m-%dT%H:%M:%S').change({hour: 23, minute: 59, second: 59})
 
     return Event.where("usuario_id = ? AND start >= ? AND start <= ?", usuario.id, data_inicio, data_fim)
+  end
+
+  def find_plantao_with_unit(unidade, data_inicio, data_fim)
+    data_inicio = DateTime.parse(data_inicio, '%Y-%m-%dT%H:%M:%S')
+    data_fim = DateTime.parse(data_fim, '%Y-%m-%dT%H:%M:%S').change({hour: 23, minute: 59, second: 59})
+
+    return Event.where("unidade_id = ? AND start >= ? AND start <= ?", unidade.id, data_inicio, data_fim)
+  end
+
+  def find_plantao_full(usuario, unidade, data_inicio, data_fim)
+    data_inicio = DateTime.parse(data_inicio, '%Y-%m-%dT%H:%M:%S')
+    data_fim = DateTime.parse(data_fim, '%Y-%m-%dT%H:%M:%S').change({hour: 23, minute: 59, second: 59})
+
+    return Event.where("usuario_id = ? AND unidade_id = ? AND start >= ? AND start <= ?", usuario.id, unidade.id, data_inicio, data_fim)
   end
 end
